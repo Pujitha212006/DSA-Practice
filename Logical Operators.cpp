028dcb543e250eaf1f66e8da6143601ebb7b9535
Pujitha Potluri <potluripujitha21@gmail.com>
2026-07-27 19:37:30 +0530

    Add Logical Operators demonstration in C++

---
+++ Logical Operators.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    cout <<(7>5)<< endl;
+    cout <<(3==5)<< endl;
+
+    cout <<((3==5)|| (7>5))<< endl;
+    cout <<((3==5)&& (7>5))<< endl;
+    return 0;
+
+}
