5243172d58782ad7250d9998e557e79f1c237e11
Pujitha Potluri <potluripujitha21@gmail.com>
2026-07-27 19:30:37 +0530

    Add Type Casting example in C++

---
+++ Type Casting.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+    double price = 100.99;
+    
+    int newPrice = (int)price; 
+    cout << "New Price: " << newPrice << endl;
+    return 0;
+
+}
