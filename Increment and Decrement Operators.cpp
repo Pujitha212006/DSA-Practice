ea2a49bd3a5a64732f3ed506718c35be6490bfd9
Pujitha Potluri <potluripujitha21@gmail.com>
2026-07-27 19:41:20 +0530

    Add example for increment and decrement operators
    Implement increment and decrement operators demonstration

---
+++ Increment and Decrement Operators.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+using namespace std;
+
+int main() {
+   
+    int a = 10;
+    int b = 10;
+
+    int x = 10;
+    int y = 10;
+
+    cout<< ++a << endl;// pre increment
+    cout<< b++ << endl;// post increment
+    
+    cout<< x-- << endl;// post decrement
+    cout<< --y << endl;// pre decrement
+    return 0;
+
+}
