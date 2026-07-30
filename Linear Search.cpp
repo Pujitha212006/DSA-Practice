c99b230ee478cb1bade1b5c074491f20715314bf
Pujitha Potluri <potluripujitha21@gmail.com>
2026-07-30 10:48:38 +0530

    Create Linear Search.cpp

---
+++ Linear Search.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+#include <climits>
+using namespace std;
+
+int linearSearch(int arr[], int sz, int target){
+    for(int i=0; i<sz; i++){
+        if(arr[i] == target){ //FOUND
+            return i;
+        }
+    }
+    return -1; //NOT FOUND
+}
+
+int main() {
+    int arr[] = {4,2,7,8,1,5,9};
+    int sz = 7;
+    int target = 8;
+
+    cout << linearSearch(arr, sz, target) << endl;
+    return 0;
+}
